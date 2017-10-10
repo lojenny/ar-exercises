23825d7ee9b6ccffc744da13e42a1f2e241f6e01
lojenny <jennychingyinlo@gmail.com>
2017-10-10 18:50:39 +0000

    Exercise 2

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -4,4 +4,12 @@ require_relative './exercise_1'
 puts "Exercise 2"
 puts "----------"
 
+@store1 = Store.find(1)
+@store2 = Store.find(2)
+@store3 = Store.find(3)
+
+
+@store1.name = 'Yaletown'
+@store1.save
+
 # Your code goes here ...
